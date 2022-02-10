2c36f383c4cf70a75e0d78a87eb2f9242e3bd246
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2022-02-10 14:11:49 +0530

    Update __main__.py

--- Dasha/__main__.py
+++ Dasha/__main__.py
@@ -1,26 +1,13 @@
-from Dasha import ubot, tbot, dbot, xbot, hbot, TOKEN_2, TOKEN, abot, bbot, cbot
+from Dasha import ubot, tbot
 import Dasha.events
 import sys
 
 try:
-  dbot.start()
-  xbot.start()
   ubot.start()
- # abot.start()
-#  cbot.start()
-#  bbot.start() 
   tbot.start(bot_token=TOKEN)
-  hbot.start(bot_token=TOKEN_2)
 except:
   print("Invalid STRING/Token__bot exiting...")
   sys.exit()
   
 ubot.run_until_disconnected()
-xbot.run_until_disconnected()
-#abot.run_until_disconnected()
-#bbot.run_until_disconnected()
-#cbot.run_until_disconnected()
-dbot.run_until_disconnected()
 tbot.run_until_disconnected()
-hbot.run_until_disconnected()
-tbot.send_message(-1001570140940, 'i am now online')
